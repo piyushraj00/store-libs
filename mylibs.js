e5378a1e58f5411355480493375d0d885c679013
piyushraj00 <80516641+piyushraj00@users.noreply.github.com>
2021-06-30 20:08:28 +0530

    Create mylibs.js

---
+++ mylibs.js
@@ -0,0 +1,19 @@
+function hello(){
+
+  Bot.sendMessage("Hello from lib!")
+
+}
+
+function goodbye(name){
+
+  Bot.sendMessage("Goodbye, " + name)
+
+}
+
+publish({
+
+  sayHello: hello,
+
+  sayGoodbyeTo: goodbye     
+
+})
